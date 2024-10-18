7a60a67fd754ca010d521ffb582e74d768e24e94
lsfernandes92 <lsfernandes92@gmail.com>
2024-10-17 23:41:46 -0300

    Add animated sleeping panda tray icon

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const { app, Tray, Menu } = require('electron')
-const path = require('path');
+const { app, Tray, Menu, nativeImage } = require("electron")
+const path = require("path");
 
 let tray = null;
 
 const icons = [
-  path.join(__dirname, './assets/panda-sleeping00.png'),
-  path.join(__dirname, './assets/panda-sleeping01.png'),
-  path.join(__dirname, './assets/panda-sleeping02.png'),
-  path.join(__dirname, './assets/panda-sleeping03.png'),
-  path.join(__dirname, './assets/panda-sleeping04.png'),
+  path.join(__dirname, "./assets/panda-sleeping0.png"),
+  path.join(__dirname, "./assets/panda-sleeping1.png"),
+  path.join(__dirname, "./assets/panda-sleeping2.png"),
+  path.join(__dirname, "./assets/panda-sleeping3.png"),
 ];
 
 let currentFrame = 0;
 
 function updateTrayIcon() {
-  tray.setImage(icons[currentFrame]);
+  let currentFrameImage = nativeImage.createFromPath(icons[currentFrame]).resize({ width: 32, height: 32 });
+
+  tray.setImage(currentFrameImage);
   currentFrame = (currentFrame + 1) % icons.length;
 }
 
@@ -24,11 +25,10 @@ app.whenReady().then(() => {
   setInterval(updateTrayIcon, 200);
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Stop animation', click: () => { /* Do something */ } },
-    { type: 'separator' },
-    { label: 'Quit', click: () => { app.quit(); } }
+    { type: "separator" },
+    { label: "Quit", click: () => { app.quit(); } }
   ]);
 
-  tray.setToolTip('Animated panda.')
+  tray.setToolTip("Animated panda.")
   tray.setContextMenu(contextMenu)
 })
